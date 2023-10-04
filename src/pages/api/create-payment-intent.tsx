b6786ad0f7dec621503775b12f5c4eab1b181ca6
DIALLO ABOUBACAR <a.diallo3012@gmail.com>
2023-10-04 10:57:12 +0200

    add apple-pay and google-pay

--- src/pages/api/create-payment-intent.tsx
+++ src/pages/api/create-payment-intent.tsx
@@ -17,18 +17,17 @@ export default async function handler(
   const { items } = req.body;
 
   try {
-    const paymentMethodOrder =
-      process.env.NODE_ENV === "production"
-        ? ["apple_pay", "google-pay", "card"]
-        : ["card"];
+    // const paymentMethodOrder =
+    //   process.env.NODE_ENV === "production"
+    //     ? ["apple_pay", "google-pay", "card"]
+    //     : ["card"];
     // Create a PaymentIntent with the order amount and currency
     const paymentIntent = await stripe.paymentIntents.create({
       amount: calculateOrderAmount(items),
       currency: "eur",
-      // automatic_payment_methods: {
-      //   enabled: true,
-      // },
-      payment_method_types: paymentMethodOrder,
+      automatic_payment_methods: {
+        enabled: true,
+      },
     });
 
     res.status(200).json({ clientSecret: paymentIntent.client_secret });
