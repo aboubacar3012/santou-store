5a4d33e1abaae21a018872cb228e4c8a9945f2dc
DIALLO ABOUBACAR <a.diallo3012@gmail.com>
2023-10-04 20:23:06 +0200

    update

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,4 +1,8 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const env = process.env.NODE_ENV || "development";
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "http://10.188.174.232:3000/api"
+    : "http://localhost:3000/api";
 export const getProducts = async () => {
   const res = await fetch(`${API_URL}/products`);
   return res.json();
